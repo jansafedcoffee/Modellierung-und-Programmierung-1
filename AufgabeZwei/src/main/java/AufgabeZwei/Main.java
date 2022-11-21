2683d549170c01e63016b91856582113764c478a
Jan Claußnitzer <claussnitzerjan@gmail.com>
2022-11-21 09:31:17 +0100

    refactored

--- AufgabeZwei/src/main/java/AufgabeZwei/Main.java
+++ AufgabeZwei/src/main/java/AufgabeZwei/Main.java
@@ -1,13 +1,11 @@
 package AufgabeZwei;
-
-
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
-        int tripel = PythagoraeischeZahlentripel.pythagoraeischeZahlentripel(1, 1000);
-        System.out.println(tripel);
-        //PythagoraeischeZahlentripel.pythagoraeischeZahlentripel(1001, 2000);
+        int tripel;
+        tripel = PythagoraeischeZahlentripel.pythagoraeischeZahlentripel(1, 1000);
+        System.out.println("Anzahl Tripel im ersten Test: " + tripel);
+        tripel = PythagoraeischeZahlentripel.pythagoraeischeZahlentripel(1001, 2000);
+        System.out.println("Anzahl Tripel im zweiten Test: " + tripel);
     }
 
 
--- AufgabeZwei/src/main/java/AufgabeZwei/PythagoraeischeZahlentripel.java
+++ AufgabeZwei/src/main/java/AufgabeZwei/PythagoraeischeZahlentripel.java
@@ -1,27 +1,52 @@
 package AufgabeZwei;
 
-
-import javax.naming.PartialResultException;
-
 public class PythagoraeischeZahlentripel {
+    private static int[] createIntArray(int minimum, int maximum) {
+        int[] loesung = new int[maximum - minimum + 1];
+        for (int i = 0; i <= loesung.length - 1; i++) {
+            loesung[i] = minimum + i;
+        }
+        return loesung;
+    }
 
+    private static int[] quadriere(int[] ganzeZahlen) {
+        int[] quadratZahlen = new int[ganzeZahlen.length];
+        for (int i = 0; i < ganzeZahlen.length; i++) {
+            quadratZahlen[i] = ganzeZahlen[i] * ganzeZahlen[i];
+        }
+        return quadratZahlen;
+    }
+
+    private static void arrayInhaltAusgeben(int[] quadratZahlen) {
+        // Gib alle Zahlen des Arrays bis auf die letzte aus.
+        for (int i = 0; i < quadratZahlen.length - 1; i++) {
+            System.out.print(quadratZahlen[i] + ", ");
+        }
+        // Gib die letzte Zahl des Arrays in der selben Zeile aus.
+        System.out.print(quadratZahlen[quadratZahlen.length - 1]);
+        System.out.println();
+    }
+
+
+    private static boolean testeAufQuadratZahl(int zahl) {
+        return Math.sqrt(zahl) % 1 == 0;
+    }
 
 
     public static int pythagoraeischeZahlentripel(int minimum, int maximum) {
-        System.out.println("Starte Methode");
-        int[] quadratic = Calculations.quadriere(Calculations.createIntArray(minimum,maximum));
-        System.out.println("Quadratisches Array");
-        Calculations.arrayInhaltAusgeben(quadratic);
+        int[] quadratZahlen = quadriere(createIntArray((int) Math.ceil(Math.sqrt(minimum)), (int) Math.floor(Math.sqrt(maximum))));
+        arrayInhaltAusgeben(quadratZahlen);
         int anzahlTripel = 0;
-        double differenz;
-        for(int i=0; i < quadratic.length; i++){
-            for(int j=0; j < quadratic.length; j++) {
-                if(j>i) {
-                    differenz = quadratic[j] -quadratic[i];
-                    if((Math.sqrt(differenz) % 1)==0 && (differenz > quadratic[i])) {
-                        int sqrtOuter = (int) Math.sqrt(quadratic[i]);
-                        int sqrtInner = (int) Math.sqrt(quadratic[j]);
-                        System.out.println(sqrtOuter +"^2 + " +(int) Math.sqrt(differenz) +"^2 = "  + sqrtInner + "^2" );
+        int differenz;
+        for (int i = 0; i < quadratZahlen.length; i++) {
+            for (int j = 0; j < quadratZahlen.length; j++) {
+                if (j > i) {
+                    differenz = quadratZahlen[j] - quadratZahlen[i];
+                    if (testeAufQuadratZahl(differenz) && (differenz > quadratZahlen[i])) {
+                        int a = (int) Math.sqrt(quadratZahlen[i]);
+                        int b = (int) Math.sqrt(differenz);
+                        int c = (int) Math.sqrt(quadratZahlen[j]);
+                        System.out.println(a + "^2 + " + b + "^2 = " + c + "^2");
                         anzahlTripel++;
                     }
                 }
